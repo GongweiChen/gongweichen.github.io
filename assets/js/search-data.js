6592bd2d748ea88f35ab6e22446fce17820ad0ad
GongweiChen <cgwfeel@163.com>
2025-08-06 06:18:48 +0000

    Deploying to gh-pages from @ GongweiChen/gongweichen.github.io@6abe2bfe91f157f12934ba740fa51be6b1ae3e73 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "post-a-post-with-plotly-js",
+  },{id: "nav-publications",
+          title: "Publications",
+          description: "\* denotes equal contributions, † denotes corresponding authors.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
